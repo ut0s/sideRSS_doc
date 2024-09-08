b32a3596def55b28bc18c70ea6290be8cb197209
Yuto Tagashira <ut0s.dev@gmail.com>
2024-09-09 05:31:22 +0900

    update: privacy policy

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 # @file publish.sh
 # @brief
 
-file=./privacy_policy.org
+file=./privacy_policy_and_disclaimer.org
 emacs --batch --eval "(require 'org)" $file --funcall org-html-export-to-html
 mv "${file%.*}".html ./docs/index.html
